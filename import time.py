7c28ae298f75ff4bd6f58740ba5abd8f04a01b08
TyphonMedia <75556422+TyphonMedia@users.noreply.github.com>
2021-05-13 10:07:51 -0700

    Delete import time.py

--- import time.py
+++
@@ -1,9 +0,0 @@
-#import keyword is used to import modules
-import time
-import datetime
-
-current_time = datetime.datetime.now()
-now = current_time.strftime("%H:%M:%S")
-date = current_time.strftime("%d/%m/%y")
-
-print(date, now)
