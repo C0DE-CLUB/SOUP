3dba01d10dca76dc8ecafbcc46a880cb60814534
TyphonMedia <75556422+TyphonMedia@users.noreply.github.com>
2021-05-12 18:57:24 -0700

    Create lambda.py

---
+++ lambda.py
@@ -0,0 +1,5 @@
+# a lambda function is a anonymous function
+# a lambda function can take any number of arguments, but can only have one expression
+
+x = lambda a: a + 10
+print(x(5)) #When you run the code the result will print out 63
